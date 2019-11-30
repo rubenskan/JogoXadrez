4c409e6b19b29c268e0661446fa4fd441c37c8d3
rubenskan@gmail.com <jfmpp!@0171>
2019-11-30 19:23:58 -0300

    Iniciando projeto

---
+++ Xadrez/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using tabuleiro;
+
+namespace Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Posicao(3, 4);
+
+            Console.WriteLine(p);
+        }
+    }
+}
